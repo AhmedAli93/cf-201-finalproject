071cf75bf09a1088a4a15cc7ab9cfefe2c0450c5
Melanie Cohen <melaniebcohen@gmail.com>
2017-12-26 16:07:11 -0800

    html form/js logic work

--- app.js
+++ app.js
@@ -2,10 +2,67 @@
 
 // variable allHikes is equal to an array of hike objects on data.js file
 var allHikes = JSON.parse(hikes);
+var form = document.getElementById('find-hike');
 
+// receives form data from find-hike.html
+function formData(event) {
+  event.preventDefault();
 
-// We will need to:
-// sort by hike length
+  // receives value from find-hike.html form, parses value into integer
+  var lengthPref = parseInt(event.target.length.value);
+
+  lengthPreference(lengthPref);
+}
+
+// sorts hikes by length
+function lengthPreference(value) {
+  console.log(allHikes.length);
+  var lengthSortArr = [];
+
+  // receives hike length form data
+  // note: 481 hikes have null for hike length
+  if (value === 1) {
+    for(var i = 0; i < allHikes.length; i++) {
+      var hikeLength = parseInt(allHikes[i].length);
+      if (hikeLength < 5.0) {
+        lengthSortArr.push(hikeLength);
+      }
+    }
+  }
+  if (value === 2) {
+    for(var j = 0; j < allHikes.length; j++) {
+      hikeLength = parseInt(allHikes[j].length);
+      if (hikeLength >= 5.0 && hikeLength < 10.0) {
+        lengthSortArr.push(hikeLength);
+      }
+    }
+  }
+  if (value === 3) {
+    for(var k = 0; k < allHikes.length; k++) {
+      hikeLength = parseInt(allHikes[k].length);
+      if (hikeLength >= 10.0 && hikeLength < 15.0) {
+        lengthSortArr.push(hikeLength);
+      }
+    }
+  }
+  if (value === 4) {
+    for(var l = 0; l < allHikes.length; l++) {
+      hikeLength = parseInt(allHikes[l].length);
+      if (hikeLength >= 15.0 && hikeLength < 20.0) {
+        lengthSortArr.push(hikeLength);
+      }
+    }
+  }
+  if (value === 5) {
+    for(var m = 0; m < allHikes.length; m++) {
+      hikeLength = parseInt(allHikes[m].length);
+      if (hikeLength >= 20.0) {
+        lengthSortArr.push(hikeLength);
+      }
+    }
+  }
+  console.log(lengthSortArr.length);
+}
 
 
 // sort by elevation gain
@@ -19,7 +76,6 @@ function elevationGain() {
   }
   console.log('# of hikes under 50 ft elevation gain',counter);
 }
-elevationGain();
 
 // sort by distance of Seattle
 //    which means we have to figure out lat/long calculation
@@ -38,4 +94,5 @@ function hikeRating() {
   }
   console.log('# of hikes with 5.0 ratings',counter);
 }
-hikeRating();
+
+form.addEventListener('submit', formData);
