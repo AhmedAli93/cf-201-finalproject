e6d8cce905272a68e8a564d223e39f5fb57cab61
Melanie Cohen <melaniebcohen@gmail.com>
2018-01-02 15:22:51 -0800

    looking good

--- slider.js
+++ slider.js
@@ -1,16 +1,13 @@
 'use strict'
 
-
 var slider = new airSlider({
+  // enable autoplay
+  autoPlay: true,
 
-    // enable autoplay
-    autoPlay: true,
+  // autoplay interval
+  autoPlayTime: 4000,
 
-    // autoplay interval
-    autoPlayTime: 3000,
-
-    // slider size
-    width: '900px',
-    height: '600px'
-
+  // slider size
+  width: '100vw',
+  height: '100vh',
 });
