2c573cb9b3da09febbe9902c7c410417f30a1742
AhmedAli93 <Ahmed98148@gmail.com>
2018-01-02 14:34:46 -0800

    added slider

---
+++ slider.js
@@ -0,0 +1,16 @@
+'use strict'
+
+
+var slider = new airSlider({
+
+    // enable autoplay
+    autoPlay: true,
+
+    // autoplay interval
+    autoPlayTime: 3000,
+
+    // slider size
+    width: '900px',
+    height: '600px'
+
+});
